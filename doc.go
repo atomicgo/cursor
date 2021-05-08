a715e62f443637aa55fd681186284ecfb6e7644b
MarvinJWendt <git@marvinjwendt.com>
2021-05-08 22:52:29 +0200

    docs: fix typo

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
 Package cursor contains cross-platform methods to move the terminal cursor in different directions.
-This package can be used to create interactive CLI tools and games, live charts, algorithm visualization and other updatable output of any kind.
+This package can be used to create interactive CLI tools and games, live charts, algorithm visualizations and other updatable output of any kind.
 
 Special thanks to github.com/k0kubun/go-ansi which this project is based on.
 */
