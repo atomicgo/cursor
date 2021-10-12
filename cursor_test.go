4092d2d25ebf49d9d92d1c3378b06d79e166c43d
Will Madison <wmadisondev@gmail.com>
2021-10-12 10:54:15 -0400

    adds the ability to set a custom writer (#5)

--- cursor_test.go
+++ cursor_test.go
@@ -1,6 +1,7 @@
 package cursor
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 )
@@ -23,5 +24,73 @@ func TestHeightCannotBeNegative(t *testing.T) {
 	Down(10)
 	if height < 0 {
 		t.Errorf("height is negative: %d", height)
+	}
+}
+
+func TestCustomIOWriter(t *testing.T) {
+	var w bytes.Buffer
+	SetTarget(&w)
+
+	Up(2)
+	expected := "\x1b[2A"
+	actual := w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+
+	w.Reset()
+	Down(2)
+	expected = "\x1b[2B"
+	actual = w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+
+	w.Reset()
+	Right(2)
+	expected = "\x1b[2C"
+	actual = w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+
+	w.Reset()
+	Left(2)
+	expected = "\x1b[2D"
+	actual = w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+
+	w.Reset()
+	Hide()
+	expected = "\x1b[?25l"
+	actual = w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+
+	w.Reset()
+	Show()
+	expected = "\x1b[?25h"
+	actual = w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+
+	w.Reset()
+	ClearLine()
+	expected = "\x1b[2K"
+	actual = w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+
+	w.Reset()
+	HorizontalAbsolute(3)
+	expected = "\x1b[4G"
+	actual = w.String()
+	if expected != actual {
+		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 }
