1d3505e40697e0c5995d569175df80c4d2d2bcfe
Brooke Hatton <brookehatton@improbable.io>
2022-03-16 18:03:36 +0000

    proposal to add windows support to the custom target functionality

--- cursor_test.go
+++ cursor_test.go
@@ -1,9 +1,9 @@
 package cursor
 
 import (
-	"bytes"
 	"fmt"
-	"runtime"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -29,73 +29,103 @@ func TestHeightCannotBeNegative(t *testing.T) {
 }
 
 func TestCustomIOWriter(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		t.Skip("skipping these tests on windows")
+	tmpFile, err := os.CreateTemp("", "testingTmpFile-")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer os.Remove(tmpFile.Name())
 
-	var w bytes.Buffer
-	SetTarget(&w)
+	w := tmpFile
+	SetTarget(w)
 
 	Up(2)
 	expected := "\x1b[2A"
-	actual := w.String()
+	actual := getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 
-	w.Reset()
+	clearFile(t, w)
 	Down(2)
 	expected = "\x1b[2B"
-	actual = w.String()
+	actual = getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 
-	w.Reset()
+	clearFile(t, w)
 	Right(2)
 	expected = "\x1b[2C"
-	actual = w.String()
+	actual = getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 
-	w.Reset()
+	clearFile(t, w)
 	Left(2)
 	expected = "\x1b[2D"
-	actual = w.String()
+	actual = getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 
-	w.Reset()
+	clearFile(t, w)
 	Hide()
 	expected = "\x1b[?25l"
-	actual = w.String()
+	actual = getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 
-	w.Reset()
+	clearFile(t, w)
 	Show()
 	expected = "\x1b[?25h"
-	actual = w.String()
+	actual = getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 
-	w.Reset()
+	clearFile(t, w)
 	ClearLine()
 	expected = "\x1b[2K"
-	actual = w.String()
+	actual = getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
 	}
 
-	w.Reset()
+	clearFile(t, w)
 	HorizontalAbsolute(3)
 	expected = "\x1b[4G"
-	actual = w.String()
+	actual = getFileContent(t, w.Name())
 	if expected != actual {
 		t.Errorf("wanted: %v, got %v", expected, actual)
+	}
+}
+
+func getFileContent(t *testing.T, fileName string) string {
+	t.Helper()
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		t.Errorf("failed to read file contents")
+
+		return ""
+	}
+
+	return string(content)
+}
+
+func clearFile(t *testing.T, file *os.File) {
+	t.Helper()
+	err := file.Truncate(0)
+	if err != nil {
+		t.Errorf("failed to clear file")
+
+		return
+	}
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		t.Errorf("failed to clear file")
+
+		return
 	}
 }
