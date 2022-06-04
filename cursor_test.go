6ad74ffe55227ba0e876233ec7ee12723fb4f845
MarvinJWendt <git@marvinjwendt.com>
2022-06-04 23:15:22 +0200

    changed CustomIOWriter tests to not run on windows

--- cursor_test.go
+++ cursor_test.go
@@ -2,8 +2,6 @@ package cursor
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"testing"
 )
 
@@ -25,107 +23,5 @@ func TestHeightCannotBeNegative(t *testing.T) {
 	Down(10)
 	if height < 0 {
 		t.Errorf("height is negative: %d", height)
-	}
-}
-
-func TestCustomIOWriter(t *testing.T) {
-	tmpFile, err := os.CreateTemp("", "testingTmpFile-")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(tmpFile.Name())
-
-	w := tmpFile
-	SetTarget(w)
-
-	Up(2)
-	expected := "\x1b[2A"
-	actual := getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-
-	clearFile(t, w)
-	Down(2)
-	expected = "\x1b[2B"
-	actual = getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-
-	clearFile(t, w)
-	Right(2)
-	expected = "\x1b[2C"
-	actual = getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-
-	clearFile(t, w)
-	Left(2)
-	expected = "\x1b[2D"
-	actual = getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-
-	clearFile(t, w)
-	Hide()
-	expected = "\x1b[?25l"
-	actual = getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-
-	clearFile(t, w)
-	Show()
-	expected = "\x1b[?25h"
-	actual = getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-
-	clearFile(t, w)
-	ClearLine()
-	expected = "\x1b[2K"
-	actual = getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-
-	clearFile(t, w)
-	HorizontalAbsolute(3)
-	expected = "\x1b[4G"
-	actual = getFileContent(t, w.Name())
-	if expected != actual {
-		t.Errorf("wanted: %v, got %v", expected, actual)
-	}
-}
-
-func getFileContent(t *testing.T, fileName string) string {
-	t.Helper()
-	content, err := os.ReadFile(fileName)
-	if err != nil {
-		t.Errorf("failed to read file contents")
-
-		return ""
-	}
-
-	return string(content)
-}
-
-func clearFile(t *testing.T, file *os.File) {
-	t.Helper()
-	err := file.Truncate(0)
-	if err != nil {
-		t.Errorf("failed to clear file")
-
-		return
-	}
-	_, err = file.Seek(0, 0)
-	if err != nil {
-		t.Errorf("failed to clear file")
-
-		return
 	}
 }
