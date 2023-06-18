a812c8e504a2620d6f2c95dcc1186d1efdf4fb22
Brooke Hatton <brooke@ravio.com>
2023-06-18 18:16:29 +0100

    Add custom writer for area (#7)
    Co-authored-by: Brooke Hatton <brookehatton@improbable.io>
    Co-authored-by: MarvinJWendt <git@marvinjwendt.com>

--- area.go
+++ area.go
@@ -2,6 +2,7 @@ package cursor
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -9,16 +10,28 @@ import (
 // You can use this to create live output, charts, dropdowns, etc.
 type Area struct {
 	height int
+	writer Writer
 }
 
 // NewArea returns a new Area.
 func NewArea() Area {
-	return Area{}
+	return Area{
+		writer: os.Stdout,
+		height: 0,
+	}
+}
+
+// WithWriter sets a custom writer for the Area.
+func (area Area) WithWriter(writer Writer) Area {
+	area.writer = writer
+
+	return area
 }
 
 // Clear clears the content of the Area.
 func (area *Area) Clear() {
 	Bottom()
+
 	if area.height > 0 {
 		ClearLinesUp(area.height)
 	}
@@ -26,9 +39,13 @@ func (area *Area) Clear() {
 
 // Update overwrites the content of the Area.
 func (area *Area) Update(content string) {
-	area.Clear()
-	fmt.Println(content)
-	height = 0
+	oldWriter := target
 
+	SetTarget(area.writer) // Temporary set the target to the Area's writer so we can use the cursor functions
+	area.Clear()
+	SetTarget(oldWriter) // Reset the target to the old writer
+	fmt.Fprintln(area.writer, content)
+
+	height = 0
 	area.height = len(strings.Split(content, "\n"))
 }
